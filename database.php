ef7d0d3f50ff81dae96e39bd6330ca7049db24b6
Charli87 <b3016598@my.shu.ac.uk>
2015-05-27 15:59:31 +0100

    To Do List

--- database.php
+++ database.php
@@ -1,14 +1,6 @@
 <?php
-/* define("SQLHOST", "localhost");
-define("SQLUSER", "b3016598");
-define("SQLDB", "b3016598_db1");
-define("SQLPASSWORD", "qtvkrpo8"); */
-
-
-
 function query($sql) {
 	$mysqli = mysqli_connect("localhost","root","","tasks");
-/* 	$mysqli = new mysqli(SQLHOST, SQLUSER, SQLPASSWORD, SQLDB); */
 	
 	if ($mysqli->connect_errno){
 	echo "failed to connect to Mysql: ".$mysqli->connect_error;
